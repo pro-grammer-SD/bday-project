977fd14059e3f14d8572a3532c4ae944ff14f52a
pro-grammer-SD <geniussantu1983@gmail.com>
2024-12-28 18:53:20 +0530

    Initial commit

---
+++ birthdays.py
@@ -0,0 +1,11 @@
+# Five birthday dates stored respectively in 5 different variables.
+
+bday_1 = "22/12/2013"
+bday_2 = "13/09/2011"
+bday_3 = "26/11/2014"
+bday_4 = "12/08/2013"
+bday_5 = "22/12/2016"
+
+# EXTRA: Printing each of them to make the program better.
+
+print(f"Birthdays:\n\t{bday_1}\n\t{bday_2}\n\t{bday_3}\n\t{bday_4}\n\t{bday_5}")
